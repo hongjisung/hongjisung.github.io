1a6a175ea13dedaec47f7244d572e042b9264d2f
jisung hong <rhfktj@gmail.com>
2019-06-03 23:31:32 +0900

    Change files

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Header from './Header';
-import Main from './Main';
-import ContentList from './ContentList';
-
-export {
-  Header,
-  Main,
-  ContentList
-};
--- src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Main } from '../components'
-
-const Home = () =>
-  <Main />
-
-export default Home;
